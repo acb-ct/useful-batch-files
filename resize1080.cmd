b1e383da3c62121c18f183d85eb772fcc86b6a51
Achim Barczok <acb@ct.de>
2020-06-26 13:32:00 +0200

    cleanup, name of output file contains old name now

--- resize1080.cmd
+++ resize1080.cmd
@@ -8,16 +8,9 @@ rem All rights reserved.
 rem Requires Irfanview
 
 
-rem File type
 set filetype=*.jpg
-
-rem Width
 set width=1080
-
-rem JPEG quality
 set quality=75
-
-rem temporary file
 set tmpfile="%temp%\imageinfo.txt"
 
 echo Converting...
@@ -27,7 +20,10 @@ for /r "%~1" %%a in (%filetype%) do call :Resize "%%a"
 "C:\Program Files (x86)\IrfanView\i_view32.exe" %1 /info=%tmpfile%
 set imagewidth=0
 for /f "tokens=4 delims= " %%b in ('type %tmpfile% ^| find /i "image dimensions"') do set /a imagewidth=%%b
-if "%imagewidth%" gtr "%width%" (
+if "%imagewidth%" gtr "%width%" 
+(
  echo Resizing file "%~1"
- "C:\Program Files (x86)\IrfanView\i_view32.exe" "%~1" /resize_long=%width% /aspectratio /resample /jpgq=%quality% /convert="C:\Users\%USERNAME%\Desktop\upload.jpg"
+ "C:\Program Files (x86)\IrfanView\i_view32.exe" "%~1" 
+ /resize_long=%width% /aspectratio /resample /jpgq=%quality% 
+ /convert="C:\Users\%USERNAME%\Desktop\%~n1-upload.jpg"
 )
